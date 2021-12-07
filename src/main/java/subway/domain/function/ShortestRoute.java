79c633329a9f56c3b3b3beecc272404958e709c9
jochungbeom <cndqjacndqja@gmail.com>
2021-12-07 14:06:37 +0900

    feat: 총 소요 시간 계산

--- src/main/java/subway/domain/function/ShortestRoute.java
+++ src/main/java/subway/domain/function/ShortestRoute.java
@@ -1,13 +1,23 @@
 package subway.domain.function;
 
+import static subway.view.InputView.*;
+
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import subway.domain.SectionRepository;
+import subway.service.ShortestRouteDistance;
 
 public enum ShortestRoute {
 	FIND_SHORTEST_DISTANCE("1") {
 		@Override
 		void function(Scanner scanner) {
-
+			String startStation = inputStartStation(scanner);
+			String endStation = inputEndStation(scanner);
+			List<String> shortestRoutePath = ShortestRouteDistance.findShortestRoutePath(startStation, endStation);
+			int totalDistance = SectionRepository.findShortestPathListDistance(shortestRoutePath);
+			int totalTime = SectionRepository.findShortestPathListTime(shortestRoutePath);
 		}
 	},
 	FIND_SHORTEST_TIME("2") {
@@ -24,6 +34,7 @@ public enum ShortestRoute {
 	};
 
 	private String command;
+
 	abstract void function(Scanner scanner);
 
 	ShortestRoute(String command) {
